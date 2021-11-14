1c188d3a725b578e0397d19e610ce5a40df825e0
hr10282018 <1902422119@qq.com>
2021-11-14 07:34:05 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;  //图片上传
 
 class UsersController extends Controller
 {
@@ -20,8 +21,19 @@ class UsersController extends Controller
     }
 
     //保存修改用户信息
-    public function update(UserRequest $request,User $user){
-      $user->update($request->all());
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+
+      //dd($request->avatar);   //调试文件图片上传
+      $data = $request->all();
+
+      if ($request->avatar) {   //判断是否有图片上传
+        $result = $uploader->save($request->avatar, 'avatars', $user->id);  //调用保存文件的方法
+        if ($result) {    //如果保存成功
+            $data['avatar'] = $result['path'];
+        }
+      }
+      $user->update($data);
+
       return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
