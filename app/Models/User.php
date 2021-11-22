8585009ba38ffaef1c1a2f129d5cf0f2d2750cd9
hr10282018 <1902422119@qq.com>
2021-11-22 12:12:11 +0000

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -87,4 +87,29 @@ class User extends Authenticatable implements MustVerifyEmailContract   //继承
         $this->unreadNotifications->markAsRead();
     }
 
+    // 针对后台添加用户时，密码作哈希处理-命名set+字段名+Attribute
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value; // 修改器自动调用
+    }
+    // 针对后台添加用户时，头像保存路径
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! \Str::startsWith($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
 }
